216911e37ed6778eda4eca2d1b700b2d4d5edf46
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2018-04-04 21:01:48 -0700

    Added axis to graphs

--- client/src/components/PostTimeline.vue
+++ client/src/components/PostTimeline.vue
@@ -13,10 +13,18 @@
             @mouseout="hideTooltip"
           />
         </g>
-        <g
-          :transform="`translate(0, ${height})`"
-          v-axis="xAxis"
-        />
+        <g :transform="`translate(0, ${height})`">
+          <g
+            v-axis="xAxis"
+          />
+          <text :transform="`translate(${width / 2 - 15}, 50)`">{{setting === 'days' ? 'Day (PST)' : 'Time (PST)'}}</text>
+        </g>
+        <g :transform="`translate(-10, 0)`">
+          <g
+            v-axis="yAxis"
+          />
+          <text :transform="`rotate(270 0 0)  translate(${-height / 2 - 50}, ${-50})`">Number of comments</text>
+        </g>
       </g>
     </svg>
     <div v-if="tooltip" :style="`position: absolute; left: ${tooltip.x}px; top: ${tooltip.y}px;`" class="tooltip">
@@ -39,9 +47,10 @@
 <script>
 import { scaleLinear, scalePoint, scaleLog } from 'd3-scale'
 import { max } from 'd3-array'
-import { axisBottom } from 'd3-axis'
+import { axisBottom, axisLeft } from 'd3-axis'
 import { select } from 'd3-selection'
 import { timeMinute } from 'd3-time'
+import { format } from 'd3-format'
 import { secInDay, 
   daysInWeek, 
   getDayFromTimestamp,
@@ -61,24 +70,9 @@ export default {
         ? scalePoint().domain(daysInWeek).range([0, this.width])
         : scaleLinear().domain([0, secInDay]).range([0, this.width])
     },
-    points () {
-      const maxKarma = max(this.data, d => d.score)
+    yScale () {
       const maxComments = max(this.data, d => d.numComments)
-      const yScale = scaleLog().domain([1, maxComments]).range([0, this.height]).clamp(true)
-      const radiusScale = scaleLinear().domain([0, maxKarma]).range([3, 20])
-
-      return this.data.map(d => ({
-        r: radiusScale(d.score),
-        cx: this.setting === 'days'
-          ? this.xScale(daysInWeek[(new Date(d.timestamp * 1000).getDay())])
-          : this.xScale(d.timestamp % secInDay),
-        cy: this.height - yScale(d.numComments),
-        id: d.id,
-        day: daysInWeek[(new Date(d.timestamp * 1000).getDay())],
-        time: timestampToHr(d.timestamp % secInDay),
-        score: d.score,
-        comments: d.numComments
-      }))
+      return scaleLog().domain([1, maxComments]).range([this.height, 0]).clamp(true)
     },
     xAxis () {
       return this.setting === 'days'
@@ -88,6 +82,26 @@ export default {
           .scale(this.xScale)
           .tickFormat(timestampToHr)
           .tickValues([0, 3, 6, 9, 12, 15, 18, 21, 24].map(d => d * 3600))
+    },
+    yAxis () {
+      return axisLeft().scale(this.yScale).ticks(20, ",.1s")
+    },
+    points () {
+      const maxKarma = max(this.data, d => d.score)
+      const radiusScale = scaleLinear().domain([0, maxKarma]).range([3, 20])
+
+      return this.data.map(d => ({
+        r: radiusScale(d.score),
+        cx: this.setting === 'days'
+          ? this.xScale(daysInWeek[(new Date(d.timestamp * 1000).getDay())])
+          : this.xScale(d.timestamp % secInDay),
+        cy: this.yScale(d.numComments),
+        id: d.id,
+        day: daysInWeek[(new Date(d.timestamp * 1000).getDay())],
+        time: timestampToHr(d.timestamp % secInDay),
+        score: d.score,
+        comments: d.numComments
+      }))
     },
     outerWidth () {
       return this.padding * 2 + this.width
@@ -139,5 +153,9 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 0 10px;
+}
+
+text {
+  font-weight: bold;
 }
 </style>
--- client/src/components/WordFrequency.vue
+++ client/src/components/WordFrequency.vue
@@ -11,6 +11,18 @@
           @mouseover="showTooltip($event, point)"
           @mouseout="hideTooltip"
         />
+        <g :transform="`translate(0, ${height})`">
+          <g
+            v-axis="xAxis"
+          />
+          <text :transform="`translate(${width / 2 - 15}, 30)`">Words</text>
+        </g>
+        <g :transform="`translate(0, 0)`">
+          <g
+            v-axis="yAxis"
+          />
+          <text :transform="`rotate(270 0 0)  translate(${-height / 2 - 50}, ${-50})`">Frequency</text>
+        </g>
       </g>
     </svg>
     <div v-if="tooltip" :style="`position: absolute; left: ${tooltip.x}px; top: ${tooltip.y}px;`" class="tooltip">
@@ -21,6 +33,8 @@
 
 <script>
 import { scaleLinear, scalePow } from 'd3-scale'
+import { select } from 'd3-selection'
+import { axisBottom, axisLeft } from 'd3-axis'
 
 export default {
   name: 'wordFrequency',
@@ -31,6 +45,18 @@ export default {
     }
   },
   computed: {
+    xScale () {
+      return scaleLinear().domain([0, this.sortedWords.length]).range([0, this.width])
+    },
+    yScale () {
+      return scalePow().domain([0, this.sortedWords[0].count]).range([this.height, 0])
+    },
+    xAxis () {
+      return axisBottom().scale(this.xScale).ticks(0)
+    },
+    yAxis () {
+      return axisLeft().scale(this.yScale)
+    },
     sortedWords () {
       const sortedWords = {}
       this.data.forEach(comment => {
@@ -50,13 +76,10 @@ export default {
         .sort((a, b) => b.count - a.count)
     },
     points () {
-      const sortedWords = this.sortedWords
-      const yScale = scalePow().domain([0, sortedWords[0].count]).range([0, this.height])
-      const xScale = scaleLinear().domain([0, sortedWords.length]).range([0, this.width])
-      return sortedWords.map((d, i) => ({
+      return this.sortedWords.map((d, i) => ({
         r: 3,
-        cx: xScale(i),
-        cy: this.height - yScale(d.count),
+        cx: this.xScale(i),
+        cy: this.yScale(d.count),
         word: d.word,
         count: d.count
       }))
@@ -80,6 +103,11 @@ export default {
       this.tooltip = null
     }
   },
+  directives: {
+    axis (el, binding) {
+      select(el).call(binding.value)
+    }
+  },
   props: ['data', 'width', 'height', 'padding']
 }
 </script>
@@ -97,5 +125,9 @@ export default {
   justify-content: center;
   height: 30px;
   border-radius: 5px;
+}
+
+text {
+  font-weight: bold;
 }
 </style>
