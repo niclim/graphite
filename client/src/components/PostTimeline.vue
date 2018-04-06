acebce5c507c81bc4ff55115bed70c737cb2fd63
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2018-04-05 17:46:09 -0700

    added transition

--- client/src/components/PostTimeline.vue
+++ client/src/components/PostTimeline.vue
@@ -4,7 +4,7 @@
       <g :transform="`translate(${padding}, ${padding})`">
         <g>
           <circle
-            v-for="point in points"
+            v-for="point in interpolatedPoints"
             :key="point.word"
             :r="point.r"
             :cx="point.cx"
@@ -51,6 +51,7 @@ import { axisBottom, axisLeft } from 'd3-axis'
 import { select } from 'd3-selection'
 import { timeMinute } from 'd3-time'
 import { format } from 'd3-format'
+import { interpolateObject } from 'd3-interpolate'
 import { secInDay, 
   daysInWeek, 
   getDayFromTimestamp,
@@ -61,14 +62,22 @@ export default {
   name: 'postTimeline',
   data () {
     return {
-      tooltip: null
+      tooltip: null,
+      interpolate: 1, // starts at days
+      transitionTarget: 1,
+      transitionTime: 200,
+      transitionStart: null
     }
   },
+  // https://github.com/d3/d3-interpolate - use interpolations to create
+  // use x and y to calculate based on interpolated value
+  // variable to store current position
   computed: {
-    xScale () {
-      return this.setting === 'days'
-        ? scalePoint().domain(daysInWeek).range([0, this.width])
-        : scaleLinear().domain([0, secInDay]).range([0, this.width])
+    dayXScale () {
+      return scalePoint().domain(daysInWeek).range([0, this.width])
+    },
+    timeXScale () {
+      return scaleLinear().domain([0, secInDay]).range([0, this.width])
     },
     yScale () {
       const maxComments = max(this.data, d => d.numComments)
@@ -76,12 +85,17 @@ export default {
     },
     xAxis () {
       return this.setting === 'days'
-        ? axisBottom()
-          .scale(this.xScale)
-        : axisBottom()
-          .scale(this.xScale)
-          .tickFormat(timestampToHr)
-          .tickValues([0, 3, 6, 9, 12, 15, 18, 21, 24].map(d => d * 3600))
+        ? this.dayXAxis
+        : this.timeXAxis
+    },
+    dayXAxis () {
+      return axisBottom().scale(this.dayXScale)
+    },
+    timeXAxis () {
+      return axisBottom()
+        .scale(this.timeXScale)
+        .tickFormat(timestampToHr)
+        .tickValues([0, 3, 6, 9, 12, 15, 18, 21, 24].map(d => d * 3600))
     },
     yAxis () {
       return axisLeft().scale(this.yScale).ticks(20, ",.1s")
@@ -89,12 +103,9 @@ export default {
     points () {
       const maxKarma = max(this.data, d => d.score)
       const radiusScale = scaleLinear().domain([0, maxKarma]).range([3, 20])
-
-      return this.data.map(d => ({
+      const points = this.data.map(d => ({
         r: radiusScale(d.score),
-        cx: this.setting === 'days'
-          ? this.xScale(daysInWeek[(new Date(d.timestamp * 1000).getDay())])
-          : this.xScale(d.timestamp % secInDay),
+        timestamp: d.timestamp,
         cy: this.yScale(d.numComments),
         id: d.id,
         day: daysInWeek[(new Date(d.timestamp * 1000).getDay())],
@@ -102,6 +113,21 @@ export default {
         score: d.score,
         comments: d.numComments
       }))
+
+      return {
+        days: points.map(d => ({
+          ...d,
+          cx: this.dayXScale(daysInWeek[(new Date(d.timestamp * 1000).getDay())])
+        })),
+        time: points.map(d => ({
+          ...d,
+          cx: this.timeXScale(d.timestamp % secInDay)
+        }))
+      }
+      
+    },
+    interpolatedPoints () {
+      return interpolateObject(this.points.time, this.points.days)(this.interpolate)
     },
     outerWidth () {
       return this.padding * 2 + this.width
@@ -123,11 +149,34 @@ export default {
     },
     hideTooltip () {
       this.tooltip = null
+    },
+    calculateInterpolate (timestamp) {
+      // Assume that the transitions dont start until other transitions are completed
+      if (!this.transitionStart) {
+        this.transitionStart = timestamp
+      }
+      const progress = (timestamp - this.transitionStart) / this.transitionTime
+      this.interpolate = 1 - (this.setting === 'days'
+        ? Math.max(this.transitionTarget - progress, 0)
+        : Math.min(progress, 1))
+
+      if (progress < 1) {
+        requestAnimationFrame(this.calculateInterpolate)
+      } else {
+        this.transitionStart = null
+      }
     }
   },
   directives: {
     axis (el, binding) {
       select(el).call(binding.value)
+    }
+  },
+  watch: {
+    setting (newSetting, oldSetting) {
+      // set transition target
+      this.transitionTarget = newSetting === 'days' ? 1 : 0 
+      requestAnimationFrame(this.calculateInterpolate)
     }
   },
   props: ['data', 'width', 'height', 'padding', 'setting']
